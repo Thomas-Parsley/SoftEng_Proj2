ccba0231f9a6b55f0fa693f558bd72deb2ed0afd
Katrina Wagner <wagner.katy@icloud.com>
2025-12-04 15:15:44 -0600

    4.0 model training files

---
+++ Training/4.0/train.py
@@ -0,0 +1,18 @@
+import torch
+from ultralytics import YOLO
+import torchvision
+import torchaudio
+
+
+def main():
+    dect_model = YOLO("yolov8s.pt")
+    dect_model.train(data="C:\\Users\\wagne\\Git\\SoftEng_Proj2\\Training\\4.0\\data.yaml",
+                    epochs=100, 
+                    imgsz=512, 
+                    batch=16,
+                    workers=2,
+                    freeze=10,        # <-- important: preserves cat/dog/person
+                    name="dect_model_test_results")
+
+if __name__ == "__main__":
+    main()
